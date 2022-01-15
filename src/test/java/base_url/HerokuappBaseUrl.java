efe3c73c522d07afc073ad54181e87aabf7f0521
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-01-15 20:31:09 +0100

    Api calisması

--- src/test/java/base_url/HerokuappBaseUrl.java
+++ src/test/java/base_url/HerokuappBaseUrl.java
@@ -1,4 +1,11 @@
 package base_url;
-
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
 public class HerokuappBaseUrl {
+    protected RequestSpecification spec;
+    @Before
+    public void setup(){
+        spec = new RequestSpecBuilder().setBaseUri("https://restful-booker.herokuapp.com").build();
+    }
 }
