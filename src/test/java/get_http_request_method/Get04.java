a47be4efc5e0ff507a2388ead141b4a8a6cb3dbb
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2022-01-15 22:28:05 +0100

    hatalar giderildi

--- src/test/java/get_http_request_method/Get04.java
+++ src/test/java/get_http_request_method/Get04.java
@@ -1,4 +1,56 @@
 package get_http_request_method;
 
-public class Get04 {
+import base_url.JsonPlaceHolderBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class Get04 extends JsonPlaceHolderBaseUrl {
+    /*
+        Given
+            https://jsonplaceholder.typicode.com/todos
+        When
+            I send a GET request to the Url
+        And
+            Accept type is “application/json”
+        Then
+            HTTP Status Code should be 200
+        And
+            Response format should be "application/json"
+        And
+            There should be 200 todos
+        And
+            "quis eius est sint explicabo" should be one of the todos
+        And
+            2, 7, and 9 should be among the userIds
+     */
+
+    @Test
+    public void get04() {
+        //url oluştur
+
+        spec.pathParams("bir", "todos");
+
+        //beklenen datayı ekleyiniz .kullanılmadığında eklemiyoruz
+
+        //request yolla ve response al
+
+        Response response = given().spec(spec).when().get("/{bir}");
+
+        //control et
+
+        response
+                .then()
+                .assertThat()
+                .contentType(ContentType.JSON)
+                .statusCode(200)
+                .body("id", hasSize(200))
+                .body("title", equalTo("quis eius est sint explicabo"))
+                .body("userId", hasItems(2, 7, 9))
+        ;
+
+    }
 }
---
+++ src/test/java/get_http_request_method/Get05.java
@@ -0,0 +1,15 @@
+package get_http_request_method;
+
+public class Get05 {
+     /*
+            Given
+                https://restful-booker.herokuapp.com/booking
+            When
+                User send a request to the URL
+            Then
+                Status code is 200
+            And
+               Among the data there should be someone whose firstname is "Jim" and last name is "Jones"
+ */
+
+}
